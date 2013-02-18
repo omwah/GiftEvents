e28edddbbd48417e663aa991528489e50e63c325
Omwah <omwah@towhee.org>
2013-02-17 22:34:59 -0800

    Add some ideas for Events class

--- src/main/java/com/github/omwah/giftevents/Event.java
+++ src/main/java/com/github/omwah/giftevents/Event.java
@@ -1,12 +1,27 @@
 package com.github.omwah.giftevents;
 
 import java.util.Calendar;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 
 /*
  */
 public class Event {
+    private String name;
     private Calendar date;
     private boolean belated;
     private String annoucement;
     private GiftSet gifts;
+    
+    public Event(EventsInfo eventsInfo, ConfigurationSection eventSection) {
+        
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public Calendar getDate(Player playerObj) {
+        return null;   
+    }
 }
