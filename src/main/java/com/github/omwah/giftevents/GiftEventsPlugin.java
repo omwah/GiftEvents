de1cc61b64241b75366da03b220099639b89ee88
AlexMl <alexandermlinzk@gmail.com>
2016-12-20 19:05:41 -0800

    Some clean up and dependency updates

--- src/main/java/com/github/omwah/giftevents/GiftEventsPlugin.java
+++ src/main/java/com/github/omwah/giftevents/GiftEventsPlugin.java
@@ -1,16 +1,15 @@
 package com.github.omwah.giftevents;
 
-import com.github.omwah.giftevents.gevent.BirthdayEvent;
-import com.github.omwah.giftevents.gevent.AnniversaryEvent;
-import com.github.omwah.giftevents.gevent.GiftEvent;
-import com.github.omwah.giftevents.gevent.GlobalEvent;
-import com.github.omwah.giftevents.gevent.IncrementalEvent;
+import com.github.omwah.giftevents.command.AnniversaryCommandExecutor;
 import com.github.omwah.giftevents.command.BirthdayCommandExecutor;
 import com.github.omwah.giftevents.command.EventsCommandExecutor;
-import com.github.omwah.giftevents.command.AnniversaryCommandExecutor;
-
+import com.github.omwah.giftevents.gevent.*;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,21 +19,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.event.Listener;
-import org.mcstats.Metrics;
-
-/*
+/**
  * This is the main class of GiftEventsPlugin
  */
 public class GiftEventsPlugin extends JavaPlugin {
+
     private EventsInfo events_info;
     private List<GiftEvent> events;
     private File events_config_file;
     private boolean enableIncrementalEvents;
-    
-    /*
+
+    /**
      * Enable plugin, set up commands and configuration
      */
     @Override
@@ -42,16 +37,16 @@ public class GiftEventsPlugin extends JavaPlugin {
         // Save the default config file from and events file from the resource
         // directory inside the plugin
         saveDefaultConfig();
-        
-        this.events_config_file = new File(this.getDataFolder(), this.getConfig().getString("events_file", "events.yml"));     
+
+        this.events_config_file = new File(this.getDataFolder(), this.getConfig().getString("events_file", "events.yml"));
         saveDefaultEventsFile();
-               
+
         // Load event information database for keeping track of 
         // birthdays and whether gifts have been handed out
         File db_file = new File(this.getDataFolder(), "events_info.db");
         boolean first_join_gift = this.getConfig().getBoolean("first_join_gift", false);
         this.enableIncrementalEvents = this.getConfig().getBoolean("incrementalEvents");
-        
+
         try {
             events_info = new EventsInfo(this, db_file, first_join_gift);
         } catch (SQLException ex) {
@@ -59,79 +54,79 @@ public class GiftEventsPlugin extends JavaPlugin {
             return;
         } catch (ClassNotFoundException ex) {
             this.getLogger().log(Level.SEVERE, "Could find JDBC class for EventsInfo database. Plugin can not be intialized!");
-            return;            
-        }           
-        
+            return;
+        }
+
         // Load list of events from the config file   
         if (!this.loadEvents()) {
             this.getLogger().log(Level.SEVERE, "events section not found in configuration, could not enable events");
             return;
         }
-                
+
         // Create the Listener for giving gifts to logging in players
         int max_announcements = this.getConfig().getInt("maximum_announcements", 5);
         Listener login_listener = new GiftEventsListener(this, max_announcements);
         this.getServer().getPluginManager().registerEvents(login_listener, this);
-        
+
         // Load commands for using the plugin
         loadCommands();
 
         // Is Metrics enabled?
-        if(this.getConfig().getBoolean("useMetrics")==true) {
-	        // Try and send metrics to MCStats
-	        try {
-	            Metrics metrics = new Metrics(this);
-	            metrics.start();
-	        } catch (IOException e) {
-	            getLogger().log(Level.SEVERE, "Could not send data to MCStats!");
-	        }
+        if (this.getConfig().getBoolean("useMetrics")) {
+            // Try and send metrics to MCStats
+            try {
+                Metrics metrics = new Metrics(this);
+                metrics.start();
+            } catch (IOException e) {
+                getLogger().log(Level.SEVERE, "Could not send data to MCStats!");
+            }
         }
     }
-    
-    /*
+
+    /**
      * Called when the plug-in shuts down
      */
     @Override
     public void onDisable() {
         // Close database connection
-        events_info.close();        
+        events_info.close();
     }
-    
-    /*
+
+    /**
      * Loads the default events file from the resource directory
      * Also makes sure to migrate any events from the config file intp
      * a seperate config file if migrating from an earlier version
      * where the events were kept inside the config.yml file.
      */
-    void saveDefaultEventsFile() {
+    private void saveDefaultEventsFile() {
         ConfigurationSection existing_events_section = this.getConfig().getConfigurationSection("events");
-        if(existing_events_section != null && !this.events_config_file.exists()) {
+        if (existing_events_section != null && !this.events_config_file.exists()) {
             // Migrate events from old config file, only if the events
             // file does not yet exist
             getLogger().log(Level.INFO, "Migrating events section from config.yml to: {0}", this.events_config_file);
-            
+
             YamlConfiguration events_config = new YamlConfiguration();
             events_config.set("events", existing_events_section);
-            
+
             try {
                 events_config.save(this.events_config_file);
-                
+
                 // Remove events section from config file
                 this.getConfig().set("events", null);
                 this.saveConfig();
             } catch (IOException ex) {
                 getLogger().info("Error saving new events file, falling back to using config.yml");
-                
+
                 // Fall back to config file
                 this.events_config_file = new File(this.getDataFolder(), "config.yml");
             }
 
         } else if (!this.events_config_file.exists()) {
             File resource_file = new File(this.getDataFolder(), "events.yml");
-            
+
             getLogger().log(Level.INFO, "Creating events file from default: {0}", resource_file.getName());
             saveResource(resource_file.getName(), false);
-            
+
             // Rename just in case configured file name is different
             resource_file.renameTo(this.events_config_file);
         } else if (existing_events_section != null) {
@@ -139,62 +134,57 @@ public class GiftEventsPlugin extends JavaPlugin {
         }
     }
 
-    /*
+    /**
      * Loads the events list from the config file
      */
     public boolean loadEvents() {
         // Reload the configuration in case this is being called to reload the events
         YamlConfiguration events_config;
-        events_config = YamlConfiguration.loadConfiguration(this.events_config_file); 
-        
-        if(events_config == null) {
-            getLogger().log(Level.INFO, "Error parsing config file: {0}", this.events_config_file);
-            return false;
-        }
-        
+        events_config = YamlConfiguration.loadConfiguration(this.events_config_file);
+
         // Create events objects from configuration file
         ConfigurationSection events_section = events_config.getConfigurationSection("events");
         if (events_section == null) {
             return false;
         }
-        
-        events = new ArrayList<GiftEvent>();
-        for(String event_name : events_section.getKeys(false)) {            
+
+        events = new ArrayList<>();
+        for (String event_name : events_section.getKeys(false)) {
             ConfigurationSection event_config = events_section.getConfigurationSection(event_name);
-            if(event_name.equalsIgnoreCase("birthday")) {
+            if (event_name.equalsIgnoreCase("birthday")) {
                 events.add(new BirthdayEvent(this.getLogger(), event_config, events_info));
-            } else if(event_name.equalsIgnoreCase("anniversary")) {
+            } else if (event_name.equalsIgnoreCase("anniversary")) {
                 events.add(new AnniversaryEvent(this.getLogger(), event_config, events_info));
             } else {
                 events.add(new GlobalEvent(this.getLogger(), event_config, this.getInputDateFormat()));
             }
         }
-        
-        if(this.enableIncrementalEvents) {
+
+        if (this.enableIncrementalEvents) {
             // Create incremental events from advanced-events config section
             ConfigurationSection advanced_section = events_config.getConfigurationSection("advanced-events");
-            if(advanced_section == null) {
+            if (advanced_section == null) {
                 return false;
             }
-            
-            for(String event_name : advanced_section.getKeys(false)) {
+
+            for (String event_name : advanced_section.getKeys(false)) {
                 ConfigurationSection event_config = advanced_section.getConfigurationSection(event_name);
                 events.add(new IncrementalEvent(this.getLogger(), event_config, events_info));
             }
         }
-        
+
         return true;
     }
-    
-    /*
+
+    /**
      * Loads commands used by the plugin
      */
     private void loadCommands() {
         // Load up the list of commands based on what is actually in the plugin.yml
-        for(String command_name : this.getDescription().getCommands().keySet()) {
+        for (String command_name : this.getDescription().getCommands().keySet()) {
             // set the command executor for the Command
             PluginCommand curr_cmd = this.getCommand(command_name);
-            
+
             if (command_name.equalsIgnoreCase("birthday")) {
                 curr_cmd.setExecutor(new BirthdayCommandExecutor(this, curr_cmd));
             } else if (command_name.equalsIgnoreCase("anniversary")) {
@@ -204,51 +194,51 @@ public class GiftEventsPlugin extends JavaPlugin {
             } else {
                 getLogger().log(Level.INFO, "Unknown command name in config.yml: {0}", command_name);
             }
-           
-        }        
+
+        }
     }
-    
-    /*
-     * Returns the EventsInfo databvase  
+
+    /**
+     * Returns the EventsInfo database
      */
     public EventsInfo getEventsInfo() {
         return events_info;
     }
-    
-    /*
+
+    /**
      * Get list of configured events
      */
     public List<GiftEvent> getEvents() {
         return events;
     }
 
-    /*
+    /**
      * Get the AnniversaryEvent
      */
     public AnniversaryEvent getAnniversaryEvent() {
-	for(GiftEvent event : getEvents()) {
-	    if(event instanceof AnniversaryEvent) {
-		return (AnniversaryEvent)event;
-	    }
-	}
-	return null;
+        for (GiftEvent event : getEvents()) {
+            if (event instanceof AnniversaryEvent) {
+                return (AnniversaryEvent) event;
+            }
+        }
+        return null;
     }
-    
-    /*
+
+    /**
      * Return the date format specified in the configuration file for inputting values
      */
     public SimpleDateFormat getInputDateFormat() {
         return new SimpleDateFormat(getConfig().getString("date_format.input", "MM-dd"));
     }
-    
-    /*
+
+    /**
      * Return the date format specified in the configuration file for outputting dates
      */
     public SimpleDateFormat getOutputDateFormat() {
         return new SimpleDateFormat(getConfig().getString("date_format.output", "MM-dd"));
     }
-    
+
     public boolean getIncrementalEnabled() {
-	return enableIncrementalEvents;
+        return enableIncrementalEvents;
     }
 }
--- src/main/java/com/github/omwah/giftevents/command/EventsGiveCommand.java
+++ src/main/java/com/github/omwah/giftevents/command/EventsGiveCommand.java
@@ -4,7 +4,6 @@ import com.github.omwah.giftevents.GiftEventsPlugin;
 import com.github.omwah.giftevents.gevent.GiftEvent;
 import com.github.omwah.omcommands.CommandHandler;
 import com.github.omwah.omcommands.PlayerSpecificCommand;
-
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 
@@ -25,31 +24,31 @@ public class EventsGiveCommand extends PlayerSpecificCommand {
 
 
     @SuppressWarnings("deprecation")
-	public boolean execute(CommandHandler handler, CommandSender sender, String label, String identifier, String[] args) {
-    	String player_name = getDestPlayer(handler, sender, args, 0);
+    public boolean execute(CommandHandler handler, CommandSender sender, String label, String identifier, String[] args) {
+        String player_name = getDestPlayer(handler, sender, args, 0);
         if (player_name == null) {
             // Problem getting player name, reported to user
             return false;
         }
-        
+
         String event_name = args[1];
-        
+
         // Look for matching event and award it
-        for(GiftEvent gift_event : plugin.getEvents()) {
+        for (GiftEvent gift_event : plugin.getEvents()) {
             if (gift_event.getName().matches(event_name)) {
                 OfflinePlayer player_obj = plugin.getServer().getOfflinePlayer(player_name);
-                if(player_obj == null) {
+                if (player_obj == null) {
                     sender.sendMessage("Could not find offline player: " + player_name);
                     return false;
-                } else if(player_obj.getPlayer() == null) {
+                } else if (player_obj.getPlayer() == null) {
                     sender.sendMessage("Can not give items to offline player: " + player_name);
-                    return false;                    
+                    return false;
                 }
                 gift_event.giveGifts(player_obj.getPlayer());
                 sender.sendMessage("Gave gifts of event: " + gift_event.getName() + " to: " + player_name);
             }
         }
-        
+
         return true;
     }
 }
